b9a8108762689b9771dd958276f31dadb60394a8
dungvv <dungvv>
2023-03-08 09:29:35 +0700

    8/3

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,34 +6,54 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-   public function postRegister(Request $request)
+    private $user;
+    public function __construct(User $user)
+        {
+            $this->user = $user;
+        }
+
+    public function postRegister(Request $request)
    {
-       $rules = [
-           'name' => 'unique:users|required',
-           'email'    => 'unique:users|required',
-           'password' => 'required',
-       ];
-       $input     = $request->only('name', 'email','password');
-       $validator = Validator::make($input, $rules);
 
-       if ($validator->fails()) {
+//       $rules = [
+//           'name' => 'unique:users|required',
+//           'email'    => 'unique:users|required',
+//           'password' => 'required',
+//       ];
+//       $input     = $request->only('name', 'email','password');
+//       $validator = Validator::make($input, $rules);
+//
+//       if ($validator->fails()) {
+//
+////           return redirect()->route('register');
+//           return response()->json(['success' => false, 'error' => $validator->messages()]);
+//       }
+//       $name = $request->name;
+//       $email    = $request->email;
+//       $password = $request->password;
+//       $user     = User::create(['name' => $name, 'email' => $email, 'password' => Hash::make($password)]);
 
-//           return redirect()->route('register');
-           return response()->json(['success' => false, 'error' => $validator->messages()]);
-       }
-       $name = $request->name;
-       $email    = $request->email;
-       $password = $request->password;
-       $user     = User::create(['name' => $name, 'email' => $email, 'password' => Hash::make($password)]);
+
+
+
+
+       $user = $this->user->create([
+          'name' => $request->name,
+          'email' => $request->email,
+          'password' => Hash::make($request->password)
+       ]);
 
        return redirect()->route('login');
    }
 
-   public function register(){
+   public function register()
+   {
+
        return view('auth/register');
    }
 
